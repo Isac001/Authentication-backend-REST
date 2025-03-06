c78331145946b3656bfab854a886e40e49f3b32f
Isac001 <isac.bezerra08@aluno.ifce.edu.br>
2025-03-06 00:11:31 -0300

    [AUT-004]

--- autentication_module/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *  
-
-urlpatterns = [
-    path("token/", CustomTokenObtainView.as_view(), name="toke"),
-]
--- authentication/views.py
+++ authentication/views.py
@@ -1,23 +1,23 @@
+# Django Imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+# Project Imports
 from .serializers import CustomTokenObtainSerializer
-import logging
 
-# Configurando logging para depuração
-logger = logging.getLogger(__name__)
-
+# API view for handling user authentication and token generation
 class CustomTokenObtainView(APIView):
 
+    # Handles POST requests for user authentication
     def post(self, request):
-        logger.info("Recebendo requisição de login...")
-        logger.info(f"Dados recebidos: {request.data}")
-
+        
+        # Deserialize and validate the incoming authentication data
         serializer = CustomTokenObtainSerializer(data=request.data)
 
+        # If authentication data is valid, return the generated tokens
         if serializer.is_valid():
-            logger.info("Autenticação bem-sucedida!")
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
-        logger.warning(f"Falha na autenticação: {serializer.errors}")
+        # If authentication fails, return an error response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
